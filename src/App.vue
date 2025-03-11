0712e2b6916423f2e00aa9781b42a32bf4fb5abd
Alexnander Nasokin <alexnas2python@gmail.com>
2025-03-11 14:50:41 +0300

    Make user form and user store.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.fetchUsers()
+})
 </script>
 
 <template>
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,27 +2,21 @@ import type { IUser } from '@/types'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-const defaultUsers: IUser[] = [
-  {
-    id: '123123',
-    fio: 'Alex',
-    birthday: '10.10.2010',
-    phone: '+9999999999',
-    email: 'sdf@sdfsd.erd'
-  },
-  {
-    id: '324234234',
-    fio: 'Mary',
-    birthday: '20.11.2011',
-    phone: '+9999999999',
-    email: 'sdf@sdfsd.erd'
-  }
-]
-
 export const useUserStore = defineStore('user', () => {
-  const users = ref<IUser[]>([...defaultUsers])
+  let users = ref<IUser[]>([])
+
+  const fetchUsers = () => {
+    users.value = JSON.parse(localStorage.getItem('usersData') || '[]')
+  }
+
+  const createUser = (newUser: IUser) => {
+    users.value.push(newUser)
+    localStorage.setItem('usersData', JSON.stringify(users.value))
+  }
 
   return {
-    users
+    users,
+    createUser,
+    fetchUsers
   }
 })
