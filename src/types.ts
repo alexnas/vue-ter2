54246d1c5d5b3aadf2a845bac934241a16436aa0
Alexnander Nasokin <alexnas2python@gmail.com>
2025-03-11 09:50:34 +0300

    Make users table.

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  id: string
+  fio: string
+  birthday: string
+  phone: string
+  email: string
+}
--- src/views/DataTableView.vue
+++ src/views/DataTableView.vue
@@ -1,8 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user'
+import { storeToRefs } from 'pinia'
+
+const userStore = useUserStore()
+const { users } = storeToRefs(userStore)
+</script>
 
 <template>
-  <div>
-    <h1>Data Table</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+  <div class="relative overflow-x-auto">
+    <table class="w-full text-left text-sm text-gray-500">
+      <thead class="text-gray-700">
+        <tr>
+          <th scope="col" class="px-6 py-3">ФИО</th>
+          <th scope="col" class="px-6 py-3">Дата рождения</th>
+          <th scope="col" class="px-6 py-3">Номер телефона</th>
+          <th scope="col" class="px-6 py-3">Электронная почта</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="{ id, fio, birthday, phone, email } in users" :key="id" scope="col">
+          <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-gray-900">
+            {{ fio }}
+          </th>
+          <td class="px-6 py-4">{{ birthday }}</td>
+          <td class="px-6 py-4">{{ phone }}</td>
+          <td class="px-6 py-4">{{ email }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
